a61a03977b84c42ad9a6529965099af17a6e8146
crowdbotics-apps <team@crowdbotics.com>
2024-09-18 17:16:41 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Kvcdfh
+
+admin.site.register(Kvcdfh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Kvcdfh(models.Model):
+    "Generated Model"
+    ccvh = models.BigIntegerField()
+    eotfg = models.BigIntegerField()
